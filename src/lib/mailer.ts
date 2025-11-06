e506dcdad025c86e2074eb94d236bdc421b46b2c
Edgar0bsj <edgarbsj.dev@gmail.com>
2025-11-06 01:11:02 -0300

    fix(mailer)

--- src/lib/mailer.ts
+++ src/lib/mailer.ts
@@ -3,8 +3,8 @@ import nodemailer from "nodemailer";
 export const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
-    user: process.env.EMAIL_USER, // seu e-mail
-    pass: process.env.EMAIL_PASS, // senha de app
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
   },
 });
 
